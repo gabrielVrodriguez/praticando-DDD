29e87514f5015b58e0546fb8e9cbc9731705b5dc
gabrielVrodriguez <121834753+gabrielVrodriguez@users.noreply.github.com>
2026-03-23 23:18:35 -0300

    feat: update TypeScript configuration and enhance answer question tests

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,3 +1,20 @@
-import { it } from 'vitest';
+import { it, expect, describe } from 'vitest';
+import { AnswerQuestion } from './answer-question';
 
 
+
+describe('create an answer', () => {
+
+    it('should be able to create an answer', () => {
+        const answerQuestion = new AnswerQuestion();
+
+        const answer = answerQuestion.execute({
+            questionId: 'question-id',
+            content: 'This is an answer',
+            instructorId: 'instructor-id'
+        });
+
+        expect(answer.content).toBe('This is an answer');
+
+    })
+});
